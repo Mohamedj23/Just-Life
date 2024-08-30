4c7fb337779c2163eafbf8d7dcf4f0df8dde18f2
Mohamedj23 <mohamedj239@gmail.com>
2024-08-30 12:38:14 +0200

    Initial commit

---
+++ src/main/java/com/task/justlife/JustlifeApplication.java
@@ -0,0 +1,13 @@
+package com.task.justlife;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JustlifeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JustlifeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/task/justlife/JustlifeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.task.justlife;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JustlifeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
